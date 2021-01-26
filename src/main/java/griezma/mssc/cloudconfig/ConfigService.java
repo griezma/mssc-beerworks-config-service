5bd9ac4c4117cfcb5ace4d57a1f98163eefd956b
griezma <griezma@outlook.com>
2021-01-26 16:49:51 +0100

    cloud config fixed

--- src/main/java/griezma/mssc/cloudconfig/ConfigService.java
+++ src/main/java/griezma/mssc/cloudconfig/ConfigService.java
@@ -1,17 +1,13 @@
-package griezma.mssc.beerworksconfig;
+package griezma.mssc.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class BeerworksConfigService {
-
+public class ConfigService {
     public static void main(String[] args) {
-        SpringApplication.run(BeerworksConfigService.class, args);
+        SpringApplication.run(ConfigService.class, args);
     }
-
 }
--- src/test/java/griezma/mssc/cloudconfig/MsscBeerworksConfigApplicationTests.java
+++ src/test/java/griezma/mssc/cloudconfig/MsscBeerworksConfigApplicationTests.java
@@ -1,4 +1,4 @@
-package griezma.mssc.beerworksconfig;
+package griezma.mssc.cloudconfig;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
